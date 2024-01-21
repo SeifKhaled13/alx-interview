068c32604c77e3ab8dfd238c92e601a536f44282
Seif Khaled <85445008+SeifKhaled13@users.noreply.github.com>
2024-01-21 15:24:57 +0200

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,31 @@
 #!/usr/bin/node
 
 const request = require('request');
+const arg = process.argv[2];
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+async function retPro (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
+      resolve(JSON.parse(body).name);
+      if (err) throw err;
+    });
   });
-};
+}
+
+async function chars () {
+  return new Promise(function (resolve, reject) {
+    request(`https://swapi-api.alx-tools.com/api/films/${arg}`, function (err, res, bod) {
+      resolve(JSON.parse(bod).characters);
+      if (err) throw err;
+    });
+  });
+}
+
+async function names () {
+  const thischars = await chars();
+  for (let i = 0; i < thischars.length; i++) {
+    console.log(await retPro(thischars[i]));
+  }
+}
+
+names();
